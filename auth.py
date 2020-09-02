ed6b6b951f955c8d104c22f00298829ed92b227c
yarasaleh <pyara-100@hotmail.com>
2020-09-02 15:37:07 +0300

    Edit: added new AUTH0_DOMAIN

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 
 
 
-AUTH0_DOMAIN = 'udacity-full-stack.us.auth0.com'
+AUTH0_DOMAIN = 'full-stack-project.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'agency'
 
