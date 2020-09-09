73175d120b1e483ef71405aafdda57cab04391b2
yarasaleh <pyara-100@hotmail.com>
2020-09-09 10:26:06 +0300

    test

--- app.py
+++ app.py
@@ -305,9 +305,9 @@ APP = create_app()
 # For Hyroku use:
 #  run the app and specify port manually:
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-    # app.run(host='0.0.0.0',port=8080,debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0',port=8080,debug=True)
 
 # # Local use:
 # if __name__ == '__main__':
