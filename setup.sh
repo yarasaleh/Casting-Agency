cc76daf4f502d1bb614d3c141b4b99de6ddf3204
yarasaleh <pyara-100@hotmail.com>
2020-09-09 10:19:22 +0300

    test

--- setup.sh
+++ setup.sh
@@ -2,7 +2,5 @@ export AUTH0_DOMAIN = 'full-stack-project.us.auth0.com'
 export ALGORITHMS = ["RS256"]
 export API_AUDIENCE = 'agency'
 export AUTH0_CLIENT_ID = 'SQkVLXQaukecUPozO8Ibv4onbWOv37oD'
-export DATABASE_URL= 'postgres://kawllcxtaiesjg:61c716b63b42933fb4cc0532a95f8c36be303a5e86b0bd739743085c20cf9ce9@ec2-3-95-87-221.compute-1.amazonaws.com:5432/dfrem3rdb4t99'
 export FLASK_APP=app.py
 export FLASK_ENV=development
-export SQLALCHEMY_DATABASE_URI= 'postgresql://yarasaleh@localhost:5432/agency'
