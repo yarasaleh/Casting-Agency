d6881347865e12e3bb03d0842ffbe0442e998f4d
yarasaleh <pyara-100@hotmail.com>
2020-09-09 09:03:27 +0300

    Fix: importing

--- auth.py
+++ auth.py
@@ -3,6 +3,7 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 
 
--- test_app.py
+++ test_app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import create_app
 from models import setup_db, Movies, Actors
 import json
+import os
 
 
 
